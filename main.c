748c1a153ded3a9a49a2f3216d83f0d700c5253d
ksen4ik2003 <kuz-ksen-serg@yandex.ru>
2016-03-14 18:00:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,63 +4,64 @@
 
 int main(int argc, char * argv[])
 {
-    int *pos;
-	int *neg;
-
-	pos = (int *) malloc(100);
-	neg = (int *) malloc(100);
-
-	int numz, nump, numn;
-	int temp;
-
-	numz = nump = numn = 0;
-
-	FILE *file;
+	int *pos;
+    	int *neg;
+    	pos = (int *) malloc(100);
+    	neg = (int *) malloc(100);
+    
+    	int numz, nump, numn;
+    	int temp;
+    
+    	numz = nump = numn = 0;
+    
+    	FILE *file;
 	file = fopen(argv[1], "r");
-    if (file == NULL) printf ("file not opened\n");
-    else printf ("file opened\n");
+	if (file == NULL) printf ("file not opened\n");
+    	else{
+    		printf ("file opened\n");
 
-	while (fscanf(file, "%d", &temp) ==1)
-	{
-		if (temp > 0)
+		while (fscanf(file, "%d", &temp) ==1)
 		{
-			pos[nump] = temp;
-			++nump;
-			if(sizeof(pos) / sizeof(pos[nump]) == nump)
-				pos = (int *) realloc (pos, nump + 100);
-		}
-		else
-			if(temp < 0)
+			if (temp > 0)
 			{
-				neg[numn] = temp;
-				++numn;
-				if(sizeof(neg) / sizeof(neg[numn]) == numn)
-					neg = (int *) realloc (neg, numn + 100);
+				pos[nump] = temp;
+				++nump;
+				if(sizeof(pos) / sizeof(pos[nump]) == nump)
+					pos = (int *) realloc (pos, nump + 100);
 			}
-			else ++numz;
-	}
-	fclose(file);
+			else
+				if(temp < 0)
+				{
+					neg[numn] = temp;
+					++numn;
+					if(sizeof(neg) / sizeof(neg[numn]) == numn)
+						neg = (int *) realloc (neg, numn + 100);
+				}
+				else ++numz;
+		}
+		fclose(file);
+    	}
 
-    int i;
+	int i;
 	file = fopen(argv[1], "w");
 	if (file == NULL) printf ("file not opened\n");
-    else printf ("done\n");
+		else printf ("done\n");
 
 	for(i = 0; i < numz; ++i)
 	{
-        fprintf(file, "%d", 0);
-        fprintf(file, " ");
+        	fprintf(file, "%d", 0);
+		fprintf(file, " ");
 	}
 	for(i = 0; i < numn; ++i)
 	{
-        fprintf(file, "%d", neg[i]);
-        fprintf(file, " ");
-    }
+		fprintf(file, "%d", neg[i]);
+        	fprintf(file, " "); 
+	}
 	for(i = 0; i < nump; ++i)
 	{
 		fprintf(file, "%d", pos[i]);
-        fprintf(file, " ");
-    }
+		fprintf(file, " ");
+    	}
 	fclose(file);
 
 	return 0;
