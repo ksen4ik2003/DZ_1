fd820065448bb91d3f74a3031f776c589ae2652c
ksen4ik2003 <kuz-ksen-serg@yandex.ru>
2016-03-15 18:44:07 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,65 +4,79 @@
 
 int main(int argc, char * argv[])
 {
-	int *pos;
-    	int *neg;
-    	pos = (int *) malloc(100);
-    	neg = (int *) malloc(100);
-    
-    	int numz, nump, numn;
-    	int temp;
-    
-    	numz = nump = numn = 0;
-    
+        int *pos = (int *) malloc(2);
+        pos[0] = 0;
+        pos[1] = 0;
+    	int *neg = (int *) malloc(2);
+    	neg[0] = 0;
+        neg[1] = 0;
+        int lenp = 2, lenn = 2;
+
+
+    	int numz = 0, nump = 0, numn = 0;
+    	int temp = 0;
+
     	FILE *file;
-	file = fopen(argv[1], "r");
-	if (file == NULL) printf ("file not opened\n");
-    	else{
+        file = fopen(argv[1], "r");
+        if (file == NULL)
+            printf ("file not opened\n");
+    	else
+    	{
     		printf ("file opened\n");
 
-		while (fscanf(file, "%d", &temp) ==1)
-		{
-			if (temp > 0)
+            	while (fscanf(file, "%d", &temp) ==1)
+            	{
+                	if (temp > 0)
+                	{
+                    		pos[nump] = temp;
+                    		++nump;
+                		if(lenp == nump)
+                    		{
+                        		pos = (int *) realloc (pos, sizeof(int) * (lenp *= 2));
+                        		int i = nump;
+                        		for( ; i < lenp; ++i)
+                            			pos[i] = 0;
+		                }
+                	}
+			else if(temp < 0)
 			{
-				pos[nump] = temp;
-				++nump;
-				if(sizeof(pos) / sizeof(pos[nump]) == nump)
-					pos = (int *) realloc (pos, nump + 100);
+				neg[numn] = temp;
+                    		++numn;
+                    		if(lenn == numn)
+                    		{
+                        		neg = (int *) realloc (neg, sizeof(int) * (lenn *= 2));
+                        		int i = numn;
+                        		for( ; i < lenn; ++i)
+                            		neg[i] = 0;
+                    		}
 			}
-			else
-				if(temp < 0)
-				{
-					neg[numn] = temp;
-					++numn;
-					if(sizeof(neg) / sizeof(neg[numn]) == numn)
-						neg = (int *) realloc (neg, numn + 100);
-				}
-				else ++numz;
-		}
+			else ++numz;
+		}//while (fscanf(file, "%d", &temp) ==1)
 		fclose(file);
-    	}
 
-	int i;
-	file = fopen(argv[1], "w");
-	if (file == NULL) printf ("file not opened\n");
-		else printf ("done\n");
 
-	for(i = 0; i < numz; ++i)
-	{
+        int i=0;
+        file = fopen(argv[1], "w");
+
+        for(i = 0; i < numz; ++i)
+        {
         	fprintf(file, "%d", 0);
-		fprintf(file, " ");
-	}
-	for(i = 0; i < numn; ++i)
-	{
-		fprintf(file, "%d", neg[i]);
-        	fprintf(file, " "); 
-	}
-	for(i = 0; i < nump; ++i)
-	{
-		fprintf(file, "%d", pos[i]);
-		fprintf(file, " ");
-    	}
-	fclose(file);
+            fprintf(file, " ");
+        }
+        for(i = 0; i < numn; ++i)
+        {
+            fprintf(file, "%d", neg[i]);
+        	fprintf(file, " ");
+        }
+        for(i = 0; i < nump; ++i)
+        {
+            fprintf(file, "%d", pos[i]);
+            fprintf(file, " ");
+        }
+        fclose(file);
+        free(pos);
+        free(neg);
+    }
 
 	return 0;
 }
